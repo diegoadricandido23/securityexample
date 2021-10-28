a7b40a493542745921f895e0519ce9576c387755
Diego <dcandido@ciandt.com>
2021-10-28 11:56:48 -0300

    Subindo Projeto

---
+++ src/main/java/br/com/diego/securityexample/SecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego.securityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExampleApplication.class, args);
+	}
+
+}
